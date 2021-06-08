fb5f21e8e5457e49821ad6aa53fa18b9940dfcd0
Luis Galindo <egalindo54@uabc.edu.mx>
2021-06-07 19:56:06 -0600

    El config generado con tangle funciona, falta añadir la configuracion

--- init.el
+++ init.el
@@ -1,49 +1,10 @@
-;; Tipografia
-(set-face-attribute 'default nil
-		    :font "Fira Code"
-		    :height 98 )
+(add-to-list 'backup-directory-alist
+             (cons "." "~/.emacs.d/backups/"))
 
-;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-;(load-theme 'sea-love t)
+(customize-set-variable
+ 'tramp-backup-directory-alist backup-directory-alist)
 
-;;(load-theme 'wombat)
-;;(load-theme sea)
-(load-file "~/.emacs.d/themes/sea.el")
-;; (load-theme 'soft-charcoal t)
-
-;; (custom-set-faces
-;;  '(neo-root-dir-face ((t (:foreground "#8D8D84"))))
-;;  '(neo-dir-link-face ((t (:foreground "#0000FF"))))
-;;  '(neo-file-link-face ((t (:foreground "#BA36A5")))))
-
-;; otros
-;;(set-face-background 'fringe "#ff0000")
-(setq inhibit-startup-message t)       ;Pantalla de inicio de emacs 
-(global-display-line-numbers-mode t)   ;numeros de linea
-(scroll-bar-mode -1)		       ;scroll bars visibles
-(tool-bar-mode -1)		       ;barra de herramientas visisles
-(menu-bar-mode -1)		       ;menu de herramientas visible
-(set-fringe-mode 10)	               ;espacio entre el frame y el buffer
-(global-visual-line-mode 1)	       ;separar lineas 
-(setq-default cursor-type 'bar)	       ;tipo del cursor
-
-(setq column-number-mode t)	       ;numero de columna 
-(line-number-mode t)		       ;numero de fila
-(display-time-mode 1)		       ;mostrar la hora
-(display-battery-mode -1)	       ;mostrar batteria
-
-;;(set-frame-parameter (selected-frame) 'undecorated t) ;frame visible
-;;(set-frame-parameter (selected-frame) 'alpha '(95 95)) ;fondo trasparente
-;;(add-to-list 'default-frame-alist '(alpha 85 85)) ;transparencia del borde
-                                                                          
-(global-set-key (kbd "C-x t") 'eshell)                                    
-(global-set-key (kbd "C-x j") 'neotree-toggle)                            
-(global-set-key (kbd "C-x <") 'ido-switch-buffer)                         
-(global-set-key (kbd "C-M-z") 'toggle-80-editting-columns-balanced)       
-                                                                          
-;; Make ESC quit prompts
-;;(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
+;; Fuetes
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org"   . "https://orgmode.org/elpa/")
                          ("elpa"  . "https://elpa.gnu.org/packages/")))
@@ -52,21 +13,25 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Initialize use-package on non-Linux platforms
+;; Inicializar 'use-package' para plataformas no unix
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
 (require 'use-package)
 
+;; Asegurarse de que los paquetes simepre esten instalados
 (setq use-package-always-ensure t)
 
+;; Mantiene una lista con los archivos abiertos recientemente es necesario que este activado.
 (require 'recentf)
 (recentf-mode 1)
 
+;; Autocompletado en el minibufer
 (use-package ivy
   :config
-  (ivy-mode 1))			     ;Activar ivy en todos los buffers
+  (ivy-mode 1))                      ;Activar ivy en todos los buffers
 
+;; explorador de archivos 
 (use-package neotree
   :config
   (setq neo-theme 'ascii)
@@ -74,16 +39,82 @@
   (setq neo-window-width 25)
   (setq neo-window-fixed-size -1))
 
+;; desactivar los numeros y el warp de texto
 (add-hook 'neo-after-create-hook
-	  (lambda (&rest _) 
-	    (display-line-numbers-mode -1)
-	    (visual-line-mode -1)))
+          (lambda (&rest _) 
+            (display-line-numbers-mode -1)
+            (visual-line-mode -1)))
 
+;; Colorear los valores RGB
 (use-package rainbow-mode)
 
+;; Getor de git para emacs
 (use-package magit)
 
+;; Collection of Ridiculously Useful eXtensions for Emacs
 (use-package crux)
+
+;; eshell hooks
+(add-hook 'eshell-mode-hook
+          (lambda (&rest _) 
+            (display-line-numbers-mode -1)
+              (visual-line-mode -1)))
+
+(use-package org
+  :bind
+  (:map org-mode-map
+        ("<M-return>" . org-toggle-latex-fragment))
+  :config
+  (setq org-support-shift-select t)
+  (setq org-format-latex-options
+        (plist-put org-format-latex-options :scale 1.5)))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (org-indent-mode t)
+            (setq org-content 2)
+            (display-line-numbers-mode -1)))
+
+;; Tipografia
+(set-face-attribute 'default nil
+                    :font "Fira Code"
+                    :height 98 )
+
+;; Tema
+(load-file "~/.emacs.d/themes/sea.el")
+
+;; otros
+(setq inhibit-startup-message t)     ;Pantalla de inicio de emacs 
+(global-display-line-numbers-mode t) ;numeros de linea
+(scroll-bar-mode -1)                 ;scroll bars visibles
+(tool-bar-mode -1)                   ;barra de herramientas visisles
+(menu-bar-mode -1)                   ;menu de herramientas visible
+(set-fringe-mode 10)                 ;espacio entre el frame y el buffer
+(global-visual-line-mode 1)          ;separar lineas 
+(setq-default cursor-type 'bar)      ;tipo del cursor
+
+;; Mode line
+(setq column-number-mode t)          ;numero de columna 
+(line-number-mode t)                 ;numero de fila
+(display-time-mode 1)                ;mostrar la hora
+(display-battery-mode -1)            ;mostrar batteria
+
+;; Frame
+;;(set-frame-parameter (selected-frame) 'undecorated t) ;frame visible
+;;(set-frame-parameter (selected-frame) 'alpha '(95 95)) ;fondo trasparente
+;;(add-to-list 'default-frame-alist '(alpha 85 85)) ;transparencia del borde
+
+;; Varios
+;;(desktop-save-mode 1)                   ;guardar escritorio
+(find-file "~/notes.org")               ;abrir archivo al iniciar
+
+;; 
+(global-set-key (kbd "C-x t") 'eshell)                                    
+(global-set-key (kbd "C-x j") 'neotree-toggle)                            
+(global-set-key (kbd "C-x <") 'ido-switch-buffer)                         
+(global-set-key (kbd "C-M-z") 'toggle-80-editting-columns-balanced)      
+
+;; Crux
 (global-set-key (kbd "C-c f") 'crux-recentf-find-file)
 (global-set-key (kbd "C-,") 'crux-find-user-init-file)
 (global-set-key (kbd "C-x C-u") 'crux-upcase-region)
@@ -91,53 +122,16 @@
 (global-set-key (kbd "C-x M-c") 'crux-capitalize-region)
 (global-set-key (kbd "C-c k") 'crux-kill-other-buffers)
 
+(defun kill-other-buffers ()
+  "Kill all other buffers."
+  (interactive)
+  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
 
-(use-package org
-  :bind
-  (:map org-mode-map
-  	("<M-return>" . org-toggle-latex-fragment))
-  ;; :hook
-  ;; (org-mode-hook . (lambda ()
-  ;; 		     (display-line-numbers-mode -1)
-  ;; 		     (org-indent-mode t)
-  ;; 		     (org-content 2)))
-  :config
-  (setq org-support-shift-select t)
-  (setq org-format-latex-options
-	(plist-put org-format-latex-options :scale 1.5)))
-
-;; (require 'org)
-;; (define-key org-mode-map (kbd "<M-return>") 'org-toggle-latex-fragment)
-
-(add-hook 'org-mode-hook
-	  (lambda ()
-	    ;; (setq org-support-shift-select t) 
-	    ;; (setq org-format-latex-options
-	    ;; 	  (plist-put org-format-latex-options :scale 1.5)) 
-
-	    (org-indent-mode t)
-	    (setq org-content 2)
-	    ;; (org-reload)
-	    (display-line-numbers-mode -1)
-
-	    )
-	  )
-
-(add-hook 'eshell-mode-hook
-	  (lambda (&rest _) 
-	    (display-line-numbers-mode -1)
-	    (visual-line-mode -1)))
-
-;; (defun kill-other-buffers ()
-;;   "kill all other buffers."
-;;   (interactive)
-;;   (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
-
-;; (defun insert-current-date () (interactive)
-;;        (insert (shell-command-to-string "echo -n $(date +%y-%m-%d)")))
+(defun insert-current-date () (interactive)
+  (insert (shell-command-to-string "echo -n $(date +%Y-%m-%d)")))
 
 (defun toggle-80-editting-columns ()
-  "set the right window margin so the edittable space is only 80 columns."
+  "Set the right window margin so the edittable space is only 80 columns."
   (interactive)
   (let ((margins (window-margins)))
     (if (or (car margins) (cdr margins))
@@ -145,7 +139,7 @@
       (set-window-margins nil 0 (max (- (window-width) 80) 0)))))
 
 (defun toggle-80-editting-columns-balanced ()
-  "set both window margins so the edittable space is only 80 columns."
+  "Set both window margins so the edittable space is only 80 columns."
   (interactive)
   (let ((margins (window-margins)))
     (if (or (car margins) (cdr margins))
@@ -154,25 +148,3 @@
              (left (/ change 2))
              (right (- change left)))
         (set-window-margins nil left right)))))
-
-;;(desktop-save-mode 1)			;guardar escritorio
-;;(find-file "~/notes.org")	                ;abrir archivo al iniciar
-
-(add-to-list 'backup-directory-alist
-             (cons "." "~/.emacs.d/backups/"))
-
-(customize-set-variable
- 'tramp-backup-directory-alist backup-directory-alist)
-(custom-set-variables
- ;; custom-set-variables was added by custom.
- ;; if you edit it by hand, you could mess it up, so be careful.
- ;; your init file should contain only one such instance.
- ;; if there is more than one, they won't work right.
- '(package-selected-packages (quote (crux use-package rainbow-mode neotree magit ivy))))
-(custom-set-faces
- ;; custom-set-faces was added by custom.
- ;; if you edit it by hand, you could mess it up, so be careful.
- ;; your init file should contain only one such instance.
- ;; if there is more than one, they won't work right.
- )
-(put 'downcase-region 'disabled nil)

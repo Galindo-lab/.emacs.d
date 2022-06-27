877851699ed5f3dcf1a8a970dc92c9ebffa7440e
Luis Galindo <egalindo54@uabc.edu.mx>
2022-06-26 17:47:09 -0700

    'custom.el' añadido y org-mode configurado

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (format-all rainbow-delimiters hl-todo centered-window git-gutter company-quickhelp company doom-themes dashboard crux which-key neotree projectile magit ivy gcmh use-package)))
+ '(tramp-backup-directory-alist (quote (("." . "~/.emacs.d/backups/")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hl-todo ((t (:inherit hl-todo :italic t)))))

10854fef10a6f359f2997a3575de93609a62c31c
Luis Galindo <egalindo54@uabc.edu.mx>
2022-06-29 16:29:09 -0700

    comp web ya funciona

--- init.el
+++ init.el
@@ -1,23 +1,18 @@
+;;
+;;             #####    #####
+;;          ####################
+;;  ###  *###      ###     (######
+;; @######                    #######    ##
+;;    ,          #               ########
+;;             #           ##@      ##
+;;           #        *# ######
+;;          #      ####    ####
+;;        *###*####         ###
+;;        ######             ##
+;;                           #
 ;; 
-;;             #####    #####                          
-;;          ####################                
-;;  ###  *###      ###     (######             
-;; @######                    #######    ##    
-;;    ,          #               ########       
-;;             #           ##@      ##          
-;;           #        *# ######           
-;;          #      ####    ####                 
-;;        *###*####         ###                 
-;;        ######             ##                 
-;;                           #                  
-;; 
-;;        *** My GNU/Emacs Config ***        
-;; 
-;; Author: Luis E. Galindo Amaya
-;; Site:   https://galindosoft.neocities.org
-;; Github: https://github.com/Galindo-lab
-;; 
-;; lun 27 jun 2022 00:24:49
+;;      https://github.com/Galindo-lab
+;;
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org"   . "https://orgmode.org/elpa/")
@@ -38,20 +33,25 @@
 (setq-default cursor-type 'bar)         ;Tipo del cursor
 (setq-default tab-width 4)              ;Tamaño del tab
 (setq-default indent-tabs-mode nil)     ;Desactivar tabs
+
 (setq scroll-step 1)                    ;Smooth scrolling
-(setq inhibit-startup-screen t)         ;hide startup screen
-(set-fringe-mode 10)                    ;Espaciado
+(setq inhibit-startup-screen t)         ;Hide startup screen
+(setq use-dialog-box nil)               ;Disable the use of dialog boxes
+
 (global-hl-line-mode 1)                 ;Cambiar el color de la line actual
+(global-auto-revert-mode 1)             ;Reverts the buffer when its visited file changes on disk
+
+(set-fringe-mode 10)                    ;Espaciado
 (tool-bar-mode -1)                      ;Barra de herramientas visisles
 (menu-bar-mode -1)                      ;Menu de herramientas visible
 (column-number-mode t)                  ;Numero de columna en el modeline
 (line-number-mode t)                    ;Numero de fila en el modeline
 (scroll-bar-mode -1)                    ;Scroll bars visibles
-(display-time-mode -1)                  ;Mostrar la hora3
+(display-time-mode -1)                  ;Mostrar la hora
 (display-battery-mode -1)               ;Mostrar batteria
 (delete-selection-mode 1)               ;Typed text replaces the selection
 
-(setq default-frame-alist '((font . "Source Code Pro-10")))
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 (setq initial-major-mode 'fundamental-mode)
 (setq initial-scratch-message nil)
@@ -73,12 +73,12 @@
 (setq gc-cons-threshold 402653184       ;Setting garbage collection threshold
       gc-cons-percentage 0.6)
 
-(use-package recentf                    ;Archivos abiertos recientemente
+(use-package recentf
   :config
   (recentf-mode 1)
   )
 
-(use-package ivy                        ;Minibuffer completion in Emacs
+(use-package ivy
   :init
   (ivy-mode 1)
 
@@ -86,15 +86,15 @@
   ("C-x <" . ido-switch-buffer)
   )
 
-(use-package magit                      ;Integracion con git
+(use-package magit
 )
 
-(use-package eshell                     ;Terminal de emacs
+(use-package eshell
   :bind
   ("C-x t" . eshell)
   )
 
-(use-package projectile                 ;Project interaction
+(use-package projectile
   :config
   (projectile-mode +1)
 
@@ -104,7 +104,7 @@
         ("C-c p" . projectile-command-map))
   )
 
-(use-package neotree                    ;Explorador de archivos
+(use-package neotree
   :bind
   ("C-x j" . neotree-toggle)
 
@@ -116,12 +116,12 @@
         neo-show-hidden-files t)
   )
 
-(use-package which-key                  ;Display key bindings
+(use-package which-key
   :config
   (which-key-mode)
   )
 
-(use-package crux                       ;Useful interactive commands
+(use-package crux
   :bind
   ("C-c f"   . crux-recentf-find-file)
   ("C-,"     . crux-find-user-init-file)
@@ -134,7 +134,7 @@
 
 (use-package dashboard
   :config
-  (setq dashboard-startup-banner "~/.emacs.d/res/nu_35.png")
+  (setq dashboard-startup-banner "~/.emacs.d/res/nu_35.txt")
   (setq dashboard-center-content t
         dashboard-items '((recents  . 10)
                           (bookmarks . 10)))
@@ -142,12 +142,12 @@
   (dashboard-setup-startup-hook)
   )
 
-(use-package doom-themes                ;tema del editor
+(use-package doom-themes
   :config
   (load-theme 'doom-opera t)
   )
 
-(use-package company                    ;completion framework for Emacs
+(use-package company
   :config
   (setq company-idle-delay 0
         company-minimum-prefix-length 2
@@ -158,22 +158,22 @@
   (global-company-mode)
   )
 
-(use-package company-quickhelp          ;show completion pop-up
+(use-package company-quickhelp
   :config
   (company-quickhelp-mode)
   )
 
-(use-package git-gutter                 ;indicating modified lines in a file
+(use-package git-gutter
   :ensure t
 
   ;; :config
   ;; (global-git-gutter-mode +1)
   )
 
-(use-package centered-window            ;Minor mode that centers the text
+(use-package centered-window
   )
 
-(use-package hl-todo                    ;Highlight keywords
+(use-package hl-todo
   :custom-face
   (hl-todo ((t (:inherit hl-todo :italic t))))
 
@@ -186,17 +186,20 @@
   (setq hl-todo-mode 1)
   )
 
-(use-package rainbow-delimiters         ;Rainbow parentheses
+(use-package rainbow-delimiters
   :hook
   (prog-mode-hook . rainbow-delimiters-mode)
   )
 
-(use-package format-all                 ;Formatear codigo
+(use-package format-all
   )
 
 (use-package emmet-mode
   :hook
-  ((sgml-mode-hook . emmet-mode))
+  ((sgml-mode . emmet-mode))
+  )
+
+(use-package gnuplot
   )
 
 (use-package org
@@ -206,16 +209,11 @@
                 (org-content 2)))
 
   :config
-  (setq org-babel-python-command "python3"
-        org-support-shift-select t
+  (setq org-support-shift-select t
         org-preview-latex-default-process 'dvisvgm
         org-html-htmlize-output-type `nil
-        org-src-tab-acts-natively t)
-
-  (setq org-plantuml-jar-path
-        (expand-file-name "~/Programas/platinuml/plantuml-1.2022.2.jar"))
-
-  (setq org-format-latex-options
+        org-src-tab-acts-natively t
+        org-format-latex-options
         (plist-put org-format-latex-options :scale 1.5))
 
   (org-babel-do-load-languages
@@ -235,8 +233,8 @@
 
 (use-package anaconda-mode
   :hook
-  ((python-mode-hook . anaconda-mode)
-   (python-mode-hook . anaconda-eldoc-mode))
+  ((python-mode . anaconda-mode)
+   (python-mode . anaconda-eldoc-mode))
   )
 
 (use-package company-anaconda
@@ -250,5 +248,25 @@
   (add-to-list 'company-backends 'company-anaconda)
   )
 
-(use-package web-mode
+(use-package company-web
+  :init
+  (require 'rx)
+
+  :after
+  (company)
+
+  :config
+  (add-to-list 'company-backends 'company-web-html)
   )
+
+;; CUSTOM -----------------------------------------------------
+
+(setq org-babel-python-command "python3"
+
+      default-frame-alist 
+      '((font . "Source Code Pro-10"))
+
+      org-plantuml-jar-path 
+      (expand-file-name "~/Programas/platinuml/plantuml-1.2022.2.jar")
+
+      )

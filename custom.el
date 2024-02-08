671902588e9433a17936f2ea2537ddc62d00ad25
galindo <egalindo54@uabc.edu.mx>
2024-02-07 18:20:11 -0800

    mié 07 feb 2024 18:20:15

--- custom.el
+++ custom.el
@@ -14,7 +14,7 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#000000" "#3f3f3f"))
  '(objed-cursor-color "#C16069")
  '(package-selected-packages
-   '(mercury-mode haskell-mode doom-themes cmake-mode zen-mode yasnippet which-key vterm use-package telephone-line rainbow-mode rainbow-delimiters racket-mode projectile plantuml-mode org-ref neotree nasm-mode modus-themes markdown-mode magit lua-mode lorem-ipsum ivy imenu-list hl-todo gnuplot git-gutter gcmh format-all fireplace ess emmet-mode crux company-web company-quickhelp company-anaconda centered-window))
+   '(prolog-mode prog-mode flymake-swi-prolog mercury-mode haskell-mode doom-themes cmake-mode zen-mode yasnippet which-key vterm use-package telephone-line rainbow-mode rainbow-delimiters racket-mode projectile plantuml-mode org-ref neotree nasm-mode modus-themes markdown-mode magit lua-mode lorem-ipsum ivy imenu-list hl-todo gnuplot git-gutter gcmh format-all fireplace ess emmet-mode crux company-web company-quickhelp company-anaconda centered-window))
  '(rustic-ansi-faces
    ["#323334" "#C16069" "#A2BF8A" "#ECCC87" "#80A0C2" "#B58DAE" "#86C0D1" "#eceff4"])
  '(tool-bar-mode nil)
@@ -48,5 +48,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 120 :width normal))))
- '(fringe ((t (:background "#000000")))))
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 120 :width normal)))))
